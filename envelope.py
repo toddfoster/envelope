2a3d8c05e0ead7a48a2bbbaa34f18743e5774d48
Todd Foster <toddfoster@users.noreply.github.com>
2015-03-03 19:05:07 -0800

    Initial form of cgi script

---
+++ envelope.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+# Import the CGI module
+import cgi
+
+# Required header that tells the browser how to render the HTML.
+print "Content-Type: text/html\n\n"
+
+# Define function to generate HTML form.
+def generate_form():
+	print "<HTML>\n"
+	print "<HEAD><TITLE>Print an Envelope</TITLE></HEAD>\n"
+	print "<h3>Print an Envelope</h3>\n"
+	print "<FORM METHOD=post ACTION=\"envelope.py\">\n"
+
+	# TODO seed fromaddress from server/browser history
+	# TODO on-load, hide fromaddress when non-empty
+	print "<p>From:<\p>\n"
+	#	print "<div><textarea name=\"fromaddress\" cols=40 rows=5></div>\n"
+	
+	print "<p>To:</p>\n"
+	print "<div><textarea name=\"toaddress\" cols=40 rows=5></textarea></div>\n"
+
+	print "<INPUT TYPE=hidden NAME =\"action\" VALUE=\"display\">\n"
+	print "<INPUT TYPE=submit VALUE=\"Enter\">\n"
+	print "</FORM>\n"
+	print "</BODY>\n"
+	print "</HTML>\n"
+
+# Define function display data.
+def display_data(toaddress):
+	print "<HTML>\n"
+	print "<HEAD><TITLE>Envelope Printing</TITLE></HEAD>\n"
+	print "<BODY>\n"
+	print "ToAddress = ", toaddress
+	print "</BODY>\n"
+	print "</HTML>\n"
+
+# Define main function.
+def main():
+	form = cgi.FieldStorage()
+	if (form.has_key("action") and form.has_key("toaddress")):
+		if (form["action"].value == "display"):
+			display_data(form["toaddress"].value)
+	else:
+		 generate_form()
+
+# Call main function.
+main()
