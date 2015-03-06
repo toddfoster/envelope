870d4a1f2d3a13e9e6f41d45f1bdefd2e3a36ac6
Todd Foster <toddfoster@users.noreply.github.com>
2015-03-05 17:25:33 -0800

    Clean up python, rm call to bash script

--- envelope.py
+++ envelope.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python
 
-# 20140303: Figured out CGI, form, call to shell: works
-# TODO: bring bash script into python, calling enscript directly
-# TODO: output result from enscript to printing screen
 # TODO: radio button to select envelope size
 # TODO: allow entry of return address with default
 # TODO: show/hide return address box
 # TODO: remember addresses printed, allow user to select
+# Done: 
+# 20150303: Figured out CGI, form, call to shell: works
+# 20140305: bring bash script into python, calling enscript directly
+# 20150305: output result from enscript to printing screen
 
 # Import the CGI module
 import cgi
-import os
+import subprocess
 
 # Required header that tells the browser how to render the HTML.
 print "Content-Type: text/html\n\n"
 
-def generate_form():
+def generateForm():
 	print "<HTML>\n"
 	print "<HEAD><TITLE>Print an Envelope</TITLE></HEAD>\n"
 	print "<h3>Print an Envelope</h3>\n"
@@ -27,7 +28,7 @@ def generate_form():
 	#print "<div><textarea name=\"fromaddress\" cols=40 rows=5></textarea></div>\n"
 	
 	print "<p>To:</p>\n"
-	print "<div><textarea name=\"toaddress\" cols=40 rows=5></textarea></div>\n"
+	print "<div><textarea name=\"toAddress\" cols=40 rows=5></textarea></div>\n"
 
 	print "<INPUT TYPE=hidden NAME =\"action\" VALUE=\"display\">\n"
 	print "<INPUT TYPE=submit VALUE=\"Enter\">\n"
@@ -36,61 +37,68 @@ def generate_form():
 	print "</HTML>\n"
 
 
-def generateResults(toaddress):
+def generateResults(toAddress, result):
 	print "<HTML>\n"
 	print "<HEAD><TITLE>Envelope Printing</TITLE></HEAD>\n"
 	print "<BODY>\n"
-	print "<p>Printed address = </p>"
+	print "<p>Printed address:</p>"
 	print "<ul>\n"
-	param = ""
-	for line in toaddress.splitlines():
-		line = line.strip()
-		if len(line) > 0:
-			line = line.replace('"','')
-			line = line.replace('`','')
-			param = param + "\"" + line + "\" "
-			print "<li>", line, "</li>\n"
+	for line in toAddress:
+		print "<li>", line, "</li>\n"
 	print "</ul>\n"
+
+	print "<p>OS responded: ", result, "</p>\n"
 
 	print "<p></p><p><a href=\"./envelope.py\"> Print another</a></p>"
 	print "</BODY>\n"
 	print "</HTML>\n"
 
 
-def sanitizeAddress(toaddress):
-	result = ""
-	for line in toaddress.splitlines():
-		line = line.strip()
-		line = line.replace('"','')
-		line = line.replace('`','')
-		if len(line) > 0:
-			result = result + line + "\n"
+def sanitizeAddress(toAddress):
+	result = []
+	for line in toAddress.splitlines():
+		candidate = line.strip().replace('"','').replace('`','')
+		if len(candidate) > 0:
+			result.append(candidate)
 	return result
 
 
-def parametizeAddress(toaddress):
-	result = ""
-	for line in toaddress.splitlines():
-		result = result + "\"" + line + "\" "
-	return result
+def print_envelope(toAddress):
 
+	returnAddress="-"
+	returnAddressFile="/home/pi/Documents/tef/secrets/returnAddress.txt"
+	try:
+		with open(returnAddressFile) as f:
+			returnAddress = f.read()
+	except Exception:
+		pass
 
-def print_envelope(toaddress):
-	toaddress = sanitizeAddress(toaddress)
-	param = parametizeAddress(toaddress)
-	cmd = "/home/pi/bin/envstationery " + param
-	os.system(cmd)
-	generateResults(toaddress)
+	vmargin = "\n" * 6
+	hmargin = " " * 20
+	content = (returnAddress + vmargin + 
+			hmargin + ("\n" + hmargin).join(toAddress))
+
+	cmd = ["/usr/bin/enscript", "--no-header", "--landscape", 
+		"--font=CourierBold@12"]
+	cmd.append("--margins=160:0:0:425")
+	process = subprocess.Popen(cmd,
+			stdin=subprocess.PIPE, 
+			stdout=subprocess.PIPE, 
+			stderr=subprocess.PIPE)
+	return process.communicate(content)
+#, shell=False, 
 
 
 # Define main function.
 def main():
 	form = cgi.FieldStorage()
-	if (form.has_key("action") and form.has_key("toaddress")):
+	if (form.has_key("action") and form.has_key("toAddress")):
 		if (form["action"].value == "display"):
-			print_envelope(form["toaddress"].value)
+			toAddress = sanitizeAddress(form["toAddress"].value)
+			result = print_envelope(toAddress)
+			generateResults(toAddress, result)
 	else:
-		 generate_form()
+		 generateForm()
 
 # Call main function.
 main()
